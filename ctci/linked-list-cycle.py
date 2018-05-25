8c5139212d26a5437baddca54d720c52079b2e2b
Nicholas Lambourne <ndl93@live.com>
2018-05-25 14:45:15 -0600

    linked list

---
+++ ctci/linked-list-cycle.py
@@ -0,0 +1,25 @@
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as:
+
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+
+def has_cycle(head):
+    if head is None:
+        return False
+    nodes = [head]
+    current = head.next
+    while current is not None:
+        if current in nodes:
+            return True
+        else:
+            nodes.append(current)
+            current = current.next
+    return False
+
