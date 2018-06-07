f4c02f1b6b78138306d6875b0531788d754bd39d
Nicholas Lambourne <ndl93@live.com>
2018-06-07 11:56:50 +1000

    html 1

---
+++ python/regex/html-parser-1.py
@@ -0,0 +1,24 @@
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start : " + tag)
+        for attr in attrs:
+            print('-> ' + attr[0] + ' > ' + str(attr[1]))
+
+    def handle_endtag(self, tag):
+        print("End   : " + tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print("Empty : " + tag)
+        for attr in attrs:
+            print('-> ' + attr[0] + ' > ' + str(attr[1]))
+
+
+n = int(input().strip())
+string = ''
+for _ in range(n):
+    string += input()
+parser = MyHTMLParser()
+parser.feed(string)
